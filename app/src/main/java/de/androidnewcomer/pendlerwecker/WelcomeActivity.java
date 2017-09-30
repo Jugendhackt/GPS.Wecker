f88d6e10b3b573d535b971a238eba861c53944e2
Joniwa31 <jonas@mendrzyk.eu>
2017-09-30 14:10:03 +0200

    Marshmallow Permision

--- app/src/main/java/de/androidnewcomer/pendlerwecker/WelcomeActivity.java
+++ app/src/main/java/de/androidnewcomer/pendlerwecker/WelcomeActivity.java
@@ -1,24 +1,96 @@
 package de.androidnewcomer.pendlerwecker;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+
+    final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        Button WelcomeButton= (Button) findViewById(R.id.WelcomeButton);
+        final Button WelcomeButton= (Button) findViewById(R.id.WelcomeButton);
         WelcomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getBaseContext(),ParameterActivity.class);
-                startActivity(intent);
+
+
+
+
+                // Here, thisActivity is the current activity
+                if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    // Should we show an explanation?
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(WelcomeActivity.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                        // Show an explanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+
+                    } else {
+
+                        // No explanation needed, we can request the permission.
+
+                        ActivityCompat.requestPermissions(WelcomeActivity.this,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+                    }
+                }else{
+                    Intent intent=new Intent(getBaseContext(),ParameterActivity.class);
+                    startActivity(intent);
+                }
+
+
             }
         });
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+                    Intent intent=new Intent(getBaseContext(),ParameterActivity.class);
+                    startActivity(intent);
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
 }
