a783939d9a0d6bcb0a33844daa5026a040e27c5d
JonasArndt <jonas.arndt@online.de>
2017-09-30 11:54:56 +0200

    Main Activity fixed

---
+++ app/src/main/java/de/androidnewcomer/pendlerwecker/ParameterActivity.java
@@ -0,0 +1,20 @@
+package de.androidnewcomer.pendlerwecker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ParameterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parameter);
+
+
+
+
+            }
+
+
+
+}
--- app/src/main/java/de/androidnewcomer/pendlerwecker/WelcomeActivity.java
+++ app/src/main/java/de/androidnewcomer/pendlerwecker/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package de.androidnewcomer.pendlerwecker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        Button WelcomeButton= (Button) findViewById(R.id.WelcomeButton);
+        WelcomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getBaseContext(),ParameterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
