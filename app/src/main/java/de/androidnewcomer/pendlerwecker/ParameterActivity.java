ac2f06b48a2fcbec65b9edcbb3a2d3e55112307b
JonasArndt <jonas.arndt@online.de>
2017-09-30 17:00:58 +0200

    Fehlermeldung

--- app/src/main/java/de/androidnewcomer/pendlerwecker/ParameterActivity.java
+++ app/src/main/java/de/androidnewcomer/pendlerwecker/ParameterActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ParameterActivity extends AppCompatActivity {
 
@@ -19,36 +20,35 @@ public class ParameterActivity extends AppCompatActivity {
             public void onClick(View view) {
 
 
+                try {
+                    EditText NGradedit = findViewById(R.id.NGrad);
+                    EditText NMinutenedit = findViewById(R.id.NMinuten);
+                    EditText NSekundenedit = findViewById(R.id.NSekunden);
+                    EditText EGradedit = findViewById(R.id.EGrad);
+                    EditText EMinutenedit = findViewById(R.id.EMinuten);
+                    EditText ESekundenedit = findViewById(R.id.ESekunden);
+                    EditText Meteredit = findViewById(R.id.Meter);
 
-               
+                    int NGrad = Integer.parseInt(NGradedit.getText().toString());
+                    int NMinuten = Integer.parseInt(NMinutenedit.getText().toString());
+                    int NSekunden = Integer.parseInt(NSekundenedit.getText().toString());
+                    int EGrad = Integer.parseInt(EGradedit.getText().toString());
+                    int EMinuten = Integer.parseInt(EMinutenedit.getText().toString());
+                    int ESekunden = Integer.parseInt(ESekundenedit.getText().toString());
+                    int meter = Integer.parseInt(Meteredit.getText().toString());
 
+                    double lat = NMinuten / 60 + NSekunden / 3600 + NGrad;
+                    double lon = EMinuten / 60 + ESekunden / 3600 + EGrad;
 
+                    Intent intent = new Intent(getBaseContext(), Geo_Service.class);
+                    intent.putExtra("lat", lat);
+                    intent.putExtra("lon", lon);
+                    intent.putExtra("meter", meter);
+                    startService(intent);
 
-                EditText NGradedit=findViewById(R.id.NGrad);
-                EditText NMinutenedit=findViewById(R.id.NMinuten);
-                EditText NSekundenedit=findViewById(R.id.NSekunden);
-                EditText EGradedit=findViewById(R.id.EGrad);
-                EditText EMinutenedit=findViewById(R.id.EMinuten);
-                EditText ESekundenedit=findViewById(R.id.ESekunden);
-                EditText Meteredit=findViewById(R.id.Meter);
-
-                int NGrad= Integer.parseInt(NGradedit.getText().toString());
-                int NMinuten= Integer.parseInt(NMinutenedit.getText().toString());
-                int NSekunden= Integer.parseInt(NSekundenedit.getText().toString());
-                int EGrad= Integer.parseInt(EGradedit.getText().toString());
-                int EMinuten= Integer.parseInt(EMinutenedit.getText().toString());
-                int ESekunden= Integer.parseInt(ESekundenedit.getText().toString());
-                int meter=Integer.parseInt(Meteredit.getText().toString());
-
-                double lat=NMinuten/60+NSekunden/3600+NGrad;
-                double lon=EMinuten/60+ESekunden/3600+EGrad;
-
-                Intent intent = new Intent(getBaseContext(), Geo_Service.class);
-                intent.putExtra("lat",lat);
-                intent.putExtra("lon", lon);
-                intent.putExtra("meter", meter);
-                startService(intent);
-
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getBaseContext(), R.string.Fehlermeldung, Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
