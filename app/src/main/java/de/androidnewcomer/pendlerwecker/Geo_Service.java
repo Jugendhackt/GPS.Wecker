f53371ab2d11c4d8c087a28ef16af67f6e3bc651
Joniwa31 <jonas@mendrzyk.eu>
2017-09-30 12:33:55 +0200

    Android 8 Support :)

--- app/src/main/java/de/androidnewcomer/pendlerwecker/Geo_Service.java
+++ app/src/main/java/de/androidnewcomer/pendlerwecker/Geo_Service.java
@@ -8,6 +8,7 @@ import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
@@ -36,7 +37,38 @@ public class Geo_Service extends Service {
 
 
 
+        if(Build.VERSION.SDK_INT >= 26) {
+        // Für Android 8
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // The id of the channel.
+            String id = "my_channel_01";
+// The user-visible name of the channel.
+            CharSequence name = "name";
+// The user-visible description of the channel.
+            String description = "description";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(id, name, importance);
+// Configure the notification channel.
+            mChannel.setDescription(description);
+            mChannel.enableLights(true);
+// Sets the notification light color for notifications posted to this
+// channel, if the device supports this feature.
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mNotificationManager.createNotificationChannel(mChannel);
+
+        }
+
+
         return START_NOT_STICKY;
+
+    }
+
+    private void gps(){
+
 
     }
 
@@ -45,4 +77,11 @@ public class Geo_Service extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+
+
+
 }
+
+
+
