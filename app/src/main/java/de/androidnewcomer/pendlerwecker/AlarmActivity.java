1de8006d572983fca19357abdd3ab6ad9b82c0a9
JonasArndt <jonas.arndt@online.de>
2017-09-30 22:13:59 +0200

    fix

--- app/src/main/java/de/androidnewcomer/pendlerwecker/AlarmActivity.java
+++ app/src/main/java/de/androidnewcomer/pendlerwecker/AlarmActivity.java
@@ -2,10 +2,7 @@ package de.androidnewcomer.pendlerwecker;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,6 +16,7 @@ public class AlarmActivity extends AppCompatActivity {
         final MediaPlayer ring= MediaPlayer.create(AlarmActivity.this,R.raw.biebbieb);
         ring.setLooping(true);
         ring.start();
+        
 
         Button stopAlarm = findViewById(R.id.stopAlarm);
         stopAlarm.setOnClickListener(new View.OnClickListener() {
