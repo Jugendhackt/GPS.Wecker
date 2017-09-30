27f3906947d20ec768db2a4cd7a17b4a4cec2568
JonasArndt <jonas.arndt@online.de>
2017-09-30 12:12:12 +0200

    Service kann gestartet werden.

--- app/src/main/java/de/androidnewcomer/pendlerwecker/ParameterActivity.java
+++ app/src/main/java/de/androidnewcomer/pendlerwecker/ParameterActivity.java
@@ -1,7 +1,10 @@
 package de.androidnewcomer.pendlerwecker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ParameterActivity extends AppCompatActivity {
 
@@ -9,12 +12,16 @@ public class ParameterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parameter);
-
-
+        Button WelcomeButton = (Button) findViewById(R.id.WelcomeButton);
+        WelcomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), Geo_Service.class);
+                startService(intent);
 
 
             }
-
-
-
+        });
+    }
 }
+
