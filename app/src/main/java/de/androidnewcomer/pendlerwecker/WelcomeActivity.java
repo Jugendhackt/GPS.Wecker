0756878542a664a5a18e621fe49ce6b8aa673586
JonasArndt <jonas.arndt@online.de>
2017-09-30 10:36:33 +0200

    Projekt erstellt

---
+++ app/src/main/java/de/androidnewcomer/pendlerwecker/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package de.androidnewcomer.pendlerwecker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
