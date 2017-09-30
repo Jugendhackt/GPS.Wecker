bf86dca378fc75966c8ec3b0d452298002827ab5
JonasArndt <jonas.arndt@online.de>
2017-09-30 22:49:46 +0200

    fix

--- app/src/main/java/de/androidnewcomer/pendlerwecker/AlarmActivity.java
+++ app/src/main/java/de/androidnewcomer/pendlerwecker/AlarmActivity.java
@@ -1,12 +1,27 @@
 package de.androidnewcomer.pendlerwecker;
 
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class AlarmActivity extends AppCompatActivity {
+    Thread vibratorthr=new Thread(){
+        public void run() {
+            while (true){
+                Vibrator vibrator=(Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibrator.vibrate(1000);
+                    try {
+                        sleep(1500);
+                    } catch (InterruptedException e) {
+                        break;
+                }
+            }}
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +31,15 @@ public class AlarmActivity extends AppCompatActivity {
         final MediaPlayer ring= MediaPlayer.create(AlarmActivity.this,R.raw.biebbieb);
         ring.setLooping(true);
         ring.start();
-        
+        vibratorthr.start();
+
 
         Button stopAlarm = findViewById(R.id.stopAlarm);
         stopAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ring.stop();
+                vibratorthr.interrupt();
                 finish();
             }
         });
